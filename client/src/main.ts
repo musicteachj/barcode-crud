9324e09573fe3295cb70731a9741f6a980eb78bd
James Littlefield <jameslittlefield3@gmail.com>
2025-05-30 15:43:46 -0400

    add vuetify, vue-router and file adjustments

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(vuetify);
+app.use(router);
+
+app.mount("#app");
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Welcome to your Vue 3 + Vuetify 3 app!</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+// Your component logic here
+</script>
